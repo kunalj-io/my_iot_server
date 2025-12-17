6c98844403100b29e98887463e2d8fe00afc9327
kunalj-io <kunaljadhav43814@gmail.com>
2025-12-17 18:05:35 +0530

    cloud port fix

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const fs = require('fs'); // NEW: Tool to read/write files
 const app = express();
-const PORT = 3000;
+const PORT = process.env.PORT || 3000;
 
 // Enable the Frontend
 app.use(express.static('public'));
